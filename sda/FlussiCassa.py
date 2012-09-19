3d24a21d33882d68e5c3e66e880fc9bc4afdd3d9
Nicola Azzini <nicola.azzini@servabit.it>
2012-09-19 11:08:56 +0000

    lavoro su flussi

--- sda/FlussiCassa.py
+++ sda/FlussiCassa.py
@@ -149,6 +149,13 @@ def computeCashFlows(fiscalyearName, moveLineDf, accountDf, periodDf,
     df0 = periodDf[periodDf["NAM_FY"]==fiscalyearName]
     df1 = df0[["FY_DATE_START"]].drop_duplicates().reset_index()
     year = (df1["FY_DATE_START"][0]).year
+    #costruzione df per i risultati
+    incomingCodesDf = matchingsDf[["entrata_code"]].drop_duplicates().reset_index(drop=True)
+    incomingCodesList = list(incomingCodesDf["entrata_code"])
+    exitCodesDf = matchingsDf[["uscita_code"]].drop_duplicates().reset_index(drop=True)
+    exitCodesList = list(exitCodesDf["uscita_code"])
+    incomingCodesList.extend(exitCodesList)
+    cashFlowsDf = pandas.DataFrame(columns=incomingCodesList)
     #calcolo dei risultati per ogni mese
     if onlyValidatedMoves:
         moveLineDf = moveLineDf[moveLineDf["STA_MOV"]=='posted'].reset_index(drop=True)
@@ -167,19 +174,73 @@ def computeCashFlows(fiscalyearName, moveLineDf, accountDf, periodDf,
         if len(df2) > 0:
             #calcolo flussi per journal
             df3 = df2.groupby("NAM_JRN").sum()[['DBT_MVL','CRT_MVL']].reset_index()
+           
+            #funzione interna utilizzata solo in questo contesto
+            def computeCashFlowFromReconcile(cashFlowsDf, defaultFlowLine, reconcilesDf, moveLineDf, amountInCredit):
+                '''
+                @param amountInCredit: è un boolean. se True -> amount = credit - debit , se False -> viceversa
+                '''
+                reconcilesDf = reconcilesDf[["ID_MVL","DBT_REC","CRT_REC","NAM_MOV","NAM_FY","NAM_JRN","percentage"]]
+                df1 = moveLineDf.rename(columns={'CRT_MVL' : 'CRT_INV', 'DBT_MVL' : 'DBT_INV'})
+                df2 = pandas.merge(reconcilesDf,df1,on=["NAM_MOV","NAM_FY","NAM_JRN"])
+                df2 = df2[df2["ID_MVL"]!=df2["ID0_MVL"]].reset_index(drop=True)
+                if amountInCredit:
+                    df2["AMOUNT"] = df2["CRT_INV"]-df2["DBT_INV"]
+                    df2["REC_AMOUNT"] = df2["DBT_REC"]-df2["CRT_REC"]
+                else:
+                    df2["AMOUNT"] = df2["DBT_INV"]-df2["CRT_INV"]
+                    df2["REC_AMOUNT"] = df2["CRT_REC"]-df2["DBT_REC"]
+                #calcolo errori nelle scritture contabili
+                df3 = df2[df2["AMOUNT"]>df2["REC_AMOUNT"]].reset_index(drop=True)
+                if len(df3)>0 and printWarnings:
+                    print "Errore a: le seguenti move line hanno importo maggiore della move.line riconciliata. Verranno scartate dai flussi di cassa"
+                    print df3[["ID0_MVL","DAT_MVL","NAM_MOV","NAM_MVL"]]
+                #calcolo flussi
+                df4 = df2[df2["AMOUNT"]<=df2["REC_AMOUNT"]].reset_index(drop=True)
+                df4["AMOUNT"] = df4["AMOUNT"] * df4["percentage"]
+                df5 = pandas.merge(df4,matchingsDf,left_on="COD_CON",right_on="servabit_code")
+                df6 = None
+                if amountInCredit:
+                    df6 = df5[["entrata_code","AMOUNT"]]
+                    df6 = df6.rename(columns={'entrata_code' : 'code'})
+                else:
+                    df6 = df5[["uscita_code","AMOUNT"]]
+                    df6 = df6.rename(columns={'uscita_code' : 'code'})
+                df7 = df6.groupby("code").sum()[['AMOUNT']].reset_index()
+                for i in range(len(df7)):
+                    row = df7[i:i+1]
+                    code = row["code"][i]
+                    amount = row["AMOUNT"][i]
+                    cashFlowsDf[code] += amount
+
+                print cashFlowsDf
+            
             #calcolo flussi suddivisi in voci
-            df4 = df2[["NAM_MOV","NAM_FY"]]
+            df4 = df2[["NAM_MOV","NAM_FY","NAM_JRN"]]
             if len(df4) > 0:
-                df5 = pandas.merge(moveLineDf,df4,on=["NAM_MOV","NAM_FY"])
+                df5 = pandas.merge(moveLineDf,df4,on=["NAM_MOV","NAM_FY","NAM_JRN"])
                 df6 = df5[df5["TYP_CON"]!='liquidity'].reset_index(drop=True)
                 #flussi per conti 'payable'
                 df7 = df6[df6["TYP_CON"]=='payable'].reset_index(drop=True)
                 df8 = df7[df7["ID_REC"].notnull()]
-                df8 = df8[["DBT_MVL","CRT_MVL","ID_REC"]]
-                df9 = pandas.merge(moveLineDf,df8,on="ID_REC")
-                print df9[["NAM_MOV","DBT_MVL.x","DBT_MVL.y","CRT_MVL.x","CRT_MVL.y"]]
-                #df9 = df9[df9["TYP_CON"]!='payable'].reset_index(drop=True)
-                #print df9[["DBT_MVL.x","CRT_MVL.x","ID_REC","TYP_CON"]]
+                df8 = df8.rename(columns={'CRT_MVL' : 'CRT_PAYED', 'DBT_MVL' : 'DBT_PAYED'})
+                df8 = df8[["CRT_PAYED","DBT_PAYED","ID_REC","ID0_MVL"]]
+                df15 = moveLineDf.rename(columns={'CRT_MVL' : 'CRT_REC', 'DBT_MVL' : 'DBT_REC'})
+                df9 = pandas.merge(df15,df8,on="ID_REC")
+                df9 = df9[df9["ID0_MVL.x"]!=df9["ID0_MVL.y"]].reset_index(drop=True)
+                del df9["ID0_MVL.y"]
+                df9 = df9.rename(columns={'ID0_MVL.x' : 'ID_MVL'})
+                df10 = df9[df9["CRT_REC"]>0].reset_index(drop=True)
+                df11 = df9[(df9["CRT_REC"]==0) & (df9["DBT_REC"]==df9["CRT_PAYED"])].reset_index(drop=True)
+                if len(df11)>0 and printWarnings:
+                    print "Errore c: le seguenti move line hanno importo di dare >0."+\
+                            "Verranno scartata dai flussi di cassa in quanto non possono rifersi ad un pagamento su un conto payable"
+                    print df11[["ID_MVL","DAT_MVL","NAM_MOV"]]
+                if len(df10)>0:
+                    #compute reconcile percentage
+                    df10["percentage"] = df10["DBT_PAYED"]/df10["CRT_REC"]
+                    df10['percentage'] = numpy.where(df10['percentage']>1,1,df10['percentage'])
+                    computeCashFlowFromReconcile(cashFlowsDf,defaultExpensesFlowLineCode,df10,moveLineDf,False)
                 
     
 if __name__ == "__main__":
