af1270b230faf8d2f32ecd2b95904f56f14a2949
Marco Pattaro <marco.pattaro@servabit.it>
2012-09-27 09:59:56 +0000

    copied flussi_cassa from trunk

--- sre/graph.py
+++ sre/graph.py
@@ -20,6 +20,7 @@
 ##############################################################################
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from matplotlib import rc
 import logging
 from tempfile import NamedTemporaryFile, TemporaryFile
@@ -41,9 +42,36 @@ FIGSIZE = { # (w, h) in inches
     'flag': (3.180, 9.52),
 }
 
+VAL_STEP = {
+    0: 0,
+    0.5: 0.2,
+    1: 0.5,
+    2: 1,
+    8: 2,
+    15: 5,
+    30: 10,
+    60: 20,
+    120: 40,
+    150: 50,
+    300: 60,
+    500: 100,
+    1000: 200,
+    1500: 500,
+    3000: 1000,
+    6000: 2000,
+    10000: 20000,
+    12000: 4000,
+    15000: 5000,
+    30000: 6000,
+    50000: 10000,
+}
+
+WORST_COLOR = "#FFDBC1"
+BEST_COLOR = "#9BFFE6"
+
 class Graph(object):
 
-    def __init__(self, data, legend=True, size='square', fontsize=10.0, **kwargs):
+    def __init__(self, data, **kwargs):
         self._logger = logging.getLogger(type(self).__name__)
         rc('legend', **{
                 'markerscale': 1, 
@@ -51,14 +79,14 @@ class Graph(object):
                 'columnspacing': 1, 
                 'borderpad': 0,
                 })
-        self._data = data
+#        self._data = data
         self._title = data.TITLE
         self._footnote = data.FOOTNOTE
         self._df = data.DF
         self._graph = list()
-        self._fontsize = fontsize
-        self._size = size
-        self._legend = legend
+        self._fontsize = data.fontsize
+        self._size = data.size
+#        self._legend = self._data.legend
         self.parse_lm(data.LM)
         self._figure = self.make_graph()
 
@@ -92,6 +120,65 @@ class Graph(object):
 
         return leg
 
+
+    def _scatter(self, ax, y_meta):
+        ''' Buld a scatter plot.
+
+        @ param y_meta:
+        @ return: A Collection instance
+        '''
+        x_data = self._lax
+        y_data = self._df[y_meta['key']]
+        # TODO: set lable for the axes
+        val = (x_data[0], y_data[0])
+        bench = (x_data[1], y_data[1])
+        quadrant = (x_data[2], y_data[2])
+        margin = 1 # TODO: make it scaled to val
+        min_val = (x_data[0:2].min() - margin, y_data[0:2].min() - margin)
+        max_val = (x_data[0:2].min() + margin, y_data[0:2].min() + margin)
+        
+        for idx in xrange(len(quadrant)):
+            quad = quadrant[idx]
+            # if quad < 2:
+            #     width = max_val[0] - bench[0]
+            # else:
+            #     width = bench[0] - min_val[0]
+            # if quad == 1 or quad == 2:
+            #     height = bench[1] - min_val[1]
+            # else:
+            #     height = max_val[1] - bench[1]
+            if quad == 0:
+                p2 = (bench_x, bench_y)
+                w2 = (max_ax - bench_x)
+                h2 = (max_ay - bench_y)
+            elif quad == 1:
+                p2 = (bench_x, min_ay)
+                w2 = (max_ax - bench_x)
+                h2 = (bench_y - min_ay)
+            elif quad == 2:
+                p2 = (min_ax, min_ay)
+                w2 = (bench_x - min_ax)
+                h2 = (bench_y - min_ay)
+            elif quad == 3:
+                p2 = (min_ax, bench_y)
+                w2 = (bench_x - min_ax)
+                h2 = (max_ay - bench_y)        
+            color = WORST_COLOR
+            if idx == 0:
+                color = BEST_COLOR
+            ax.add_patch(
+                patches.Rectangle(
+                    bench, width=width, height=height, antialiased=True,
+                    facecolor=color, zorder=1))
+
+        return ax.scatter(val[0], val[1], s=50, c=y_meta['color'], color='DarkBlue', antialiased=True, zorder=3)
+        # TODO: where is the label?
+        # offset = 0.02
+        # text_pos(val[0]+offset, val[1]+offset)
+        # an = ax.text(text_pos, 
+        #              imp_name, color='DarkBlue', clip_on=False, zorder=4)
+        
+
     def parse_lm(self, lm):
         ''' Parse Bag's LM dictionary and estract the following non-public
         attrubutes:
@@ -102,11 +189,7 @@ class Graph(object):
         '''
         # TODO: handle line styles (see sftp://terra.devsite/home/studiabo/UlisseRep/Q20/Q20S2630/HS690790/MER_ARE)
         for key, val in lm.iteritems():
-            if key == 'size':
-                self._size = lm['size']
-            elif key == 'fontsize':
-                self._fontsize = lm['fontsize']
-            elif val[0] == 'lax':
+            if val[0] == 'lax':
                 self._lax = self._df[key]
             elif val[0] in GRAPH_TYPES:
                 self._graph.append({
@@ -155,16 +238,16 @@ class Graph(object):
                 # TODO: handle cumulative bars
                 line = ax.bar(self._lax, self._df[col['key']], color=col['color'], align='center')
             elif col['type'] == 'scatter':
-                line = ax.scatter(self._df[col['key']])
+                line = self._scatter(ax, col)
             else:
                 self._logger.warning("Unhandled graph type '%s', I will ignore entry '%s'", 
                                      col['type'], col['key'])
                 continue
             lines.append(line)
             labels.append(col['label'])
-        handles = [line[0] for line in lines]
-        if self._legend:
-            leg = self._make_legend(fig, handles, labels)
+        # if self._legend:
+        #     handles = [line[0] for line in lines]
+        #     leg = self._make_legend(fig, handles, labels)
         return fig
 
     def out(self):
@@ -221,10 +304,10 @@ if __name__ == '__main__':
 
     lm = {
         'a': ['lax'],
-        'c': ['plot', 'sx', '\LaTeX', 'r'],
-        'b': ['bar', 'dx', "$E=mc^2$", 'b'],
-        'd': ['plot', 'dx', 'Dau', 'g'],
-        }
+        'c': ['scatter', 'sx', '\LaTeX', 'r'],
+        # 'b': ['bar', 'dx', "$E=mc^2$", 'b'],
+        # 'd': ['plot', 'dx', 'Dau', 'g'],
+        }    
 
     df = pnd.DataFrame({'a':np.arange(0, 10,1)})
     df['b'] = np.random.randn(10)
@@ -240,6 +323,7 @@ if __name__ == '__main__':
         'c': [1, 'l|', '|@v0|', '\LaTeX|'],
         'd': [2, 'r|', '@v1|', 'Dau|'],
         }
+        
     path = '/home/mpattaro/workspace/star/trunk/sre/esempio/table0.pickle'
     bag = Bag(df, path, LM=lm, TITLE='Esempio')
     bag.save()
